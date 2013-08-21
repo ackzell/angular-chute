094dbf4a4fc0ce2551899f7c42bb9c2a106a92d0
Petr Bela <bela.petr@gmail.com>
2013-08-21 11:39:17 -0700

    use magnificPopup for image lightbox and submission modal

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,23 @@
 'use strict';
 
 angular.module('angularChuteApp')
-  .controller('MainCtrl', ['$scope', 'Asset', function($scope, Asset) {
+  .controller('MainCtrl', ['$scope', '$timeout', 'Asset', function($scope, $timeout, Asset) {
+
+    $('.open-submission-modal').magnificPopup({
+      type:'inline',
+      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
+
     Asset.query({album: 'abcqsrlx'}, function(assets) {
       $scope.assets = _.map(assets, function(asset) { return new Asset(asset) });
+      $timeout(function() {
+        $('.wall').magnificPopup({
+          delegate: 'a.magnific-popup',
+          type: 'image',
+          gallery: {enabled: true},
+          image: {cursor: null}
+        });
+      });
     });
 
   }]);
