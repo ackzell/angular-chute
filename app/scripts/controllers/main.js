51a3ff89d7484f891fb6a7fd750bd2647dcd21fe
Petr Bela <bela.petr@gmail.com>
2013-08-21 14:03:58 -0700

    style lightbox

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,7 +15,36 @@ angular.module('angularChuteApp')
           delegate: 'a.magnific-popup',
           type: 'image',
           gallery: {enabled: true},
-          image: {cursor: null}
+          image: {
+            // markup: '<div class="mfp-figure">'+
+            //           '<div class="mfp-close"></div>'+
+            //           '<div class="mfp-img"></div>'+
+            //           '<div class="mfp-bottom-bar">'+
+            //             '<div class="mfp-title"></div>'+
+            //             '<div class="mfp-counter"></div>'+
+            //           '</div>'+
+            //         '</div>',
+            markup: '<div class="lightbox mfp-figure">'+
+                      '<div class="mfp-close"></div>'+
+                      '<div class="border">'+
+                        '<div class="mfp-img"></div>'+
+                        '<div class="like icon-star" ng-class="{\'active\': asset.hearted()}"></div>'+
+                        '<span class="likes"><span class="likes-number">{{asset.hearts}}</span> votes</span>'+
+                        '<div class="meta">'+
+                          '<span class="username">{{asset._username()}}</span> | '+
+                          '<i class="icon-map-marker"></i>&nbsp;&nbsp;{{asset.loc()}}'+
+                        '</div>'+
+                        '<p class="mfp-title"></p>'+
+                        '<div class="sharing">'+
+                          '<a href="#" class="facebook" target="_blank"><i class="icon-facebook icon-large"></i></a>'+
+                          '<a href="#" class="twitter" target="_blank"><i class="icon-twitter icon-large"></i></a>'+
+                          '<a href="#" class="pinterest" target="_blank"><i class="icon-pinterest icon-large"></i></a>'+
+                          '<a href="#" class="tumblr" target="_blank"><i class="icon-tumblr icon-large"></i></a>'+
+                        '</div>'+
+                      '</div>'+
+                    '</div>',
+            cursor: null
+          }
         });
       });
     });
