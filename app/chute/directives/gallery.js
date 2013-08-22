0ddc58218cefc28ede40e14555c3209c6582eddb
Petr Bela <bela.petr@gmail.com>
2013-08-22 13:34:10 -0700

    enable using config object

--- app/chute/directives/gallery.js
+++ app/chute/directives/gallery.js
@@ -1,10 +1,12 @@
 'use strict';
 
-angular.module('chute').directive('chuteGallery', [function() {
+angular.module('chute').directive('chuteGallery', ['config', '$', function(config, $) {
+  config = config || {};
+
   return {   
     restrict: 'AC',
     controller: ['$scope', '$attrs', 'Asset', function($scope, $attrs, Asset) {
-      var options = $scope.$eval($attrs.chuteGallery) || {};
+      var options = $.extend(true, {}, config, $scope.$eval($attrs.chuteGallery));
 
       $scope.assets = Asset.query({album: options.album, perPage: options.perPage});
     }]
