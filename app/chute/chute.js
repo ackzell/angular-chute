6fca298533c4f8cad6d87d71f6948eef2aaade5c
Petr Bela <bela.petr@gmail.com>
2013-08-22 12:05:21 -0700

    dont fail if jquery/_ not defined

--- app/chute/chute.js
+++ app/chute/chute.js
@@ -4,7 +4,7 @@ angular.module('chute', ['ngResource']);
 
 angular.module('chute').constant('apiUrl', 'http://api.getchute.com/v2');
 
-angular.module('chute').constant('$', window.jQueryChute || jQuery);
-angular.module('chute').constant('jQuery', window.jQueryChute || jQuery);
+angular.module('chute').constant('$', window.jQueryChute || window.jQuery);
+angular.module('chute').constant('jQuery', window.jQueryChute || window.jQuery);
 
-angular.module('chute').constant('_', _);
+angular.module('chute').constant('_', window._);
