26ba9178fe450ee992989abe73523c0d7c4e0234
Petr Bela <bela.petr@gmail.com>
2013-08-22 12:01:24 -0700

    injectable jQuery ($)

--- app/chute/chute.js
+++ app/chute/chute.js
@@ -3,3 +3,6 @@
 angular.module('chute', ['ngResource']);
 
 angular.module('chute').constant('apiUrl', 'http://api.getchute.com/v2');
+
+angular.module('chute').constant('$', window.jQueryChute || jQuery);
+angular.module('chute').constant('jQuery', window.jQueryChute || jQuery);
