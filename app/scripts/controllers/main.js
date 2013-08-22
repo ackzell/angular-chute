3ac86787c6240ae223c5ee945db78f5c5d4a4726
Petr Bela <bela.petr@gmail.com>
2013-08-22 11:31:59 -0700

    first attempt at ads

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,10 @@ angular.module('CapitalOne')
       midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
     });
 
-    Asset.query({album: 'abcqsrlx'}, function(assets) {
+    Asset.query({album: 'abcqsrlx', perPage: 15}, function(assets) {
       $scope.assets = _.map(assets, function(asset) { return new Asset(asset) });
     });
+
+    $scope.ord = Math.floor(Math.random() * 1e16);
 
   }]);
