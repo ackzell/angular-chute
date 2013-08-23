35d4ad1eb98c890190ae70ec871e9144c97be0d1
Petr Bela <bela.petr@gmail.com>
2013-08-22 18:55:10 -0700

    wu.masonry as dependency in chute module

--- app/chute/chute.js
+++ app/chute/chute.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('chute', ['ngResource']);
+angular.module('chute', ['ngResource', 'wu.masonry']);
 
 angular.module('chute').constant('apiUrl', 'http://api.getchute.com/v2');
 
@@ -8,3 +8,17 @@ angular.module('chute').constant('$', window.jQueryChute || window.jQuery);
 angular.module('chute').constant('jQuery', window.jQueryChute || window.jQuery);
 
 angular.module('chute').constant('_', window._);
+
+// angular.module('chute').constant('config', {});  // allow apps to inject their config
+
+// http://better-inter.net/enabling-cors-in-angular-js/
+// angular.module('chute').config(['$httpProvider', function($httpProvider) {
+//   $httpProvider.defaults.useXDomain = true;
+//   delete $httpProvider.defaults.headers.common['X-Requested-With'];
+// }]);
+
+// angular.module('chute').config(function($provide) {
+//   $provide.decorator('config', ['$delegate', function($delegate) {
+//     return $delegate || {};
+//   }]);
+// });
