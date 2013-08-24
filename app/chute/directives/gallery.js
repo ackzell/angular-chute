503de59afed6401984372ba8dd1e088b4d1b8369
Petr Bela <bela.petr@gmail.com>
2013-08-24 12:05:03 -0700

    namespaced asset

--- app/chute/directives/gallery.js
+++ app/chute/directives/gallery.js
@@ -5,7 +5,7 @@ angular.module('chute').directive('chuteGallery', ['config', '$', function(confi
 
   return {   
     restrict: 'AC',
-    controller: ['$scope', '$attrs', 'Asset', function($scope, $attrs, Asset) {
+    controller: ['$scope', '$attrs', 'Chute.API.Asset', function($scope, $attrs, Asset) {
       var options = $.extend(true, {}, config, $scope.$eval($attrs.chuteGallery));
 
       $scope.assets = Asset.query({album: options.album, perPage: options.perPage});
