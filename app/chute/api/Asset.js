dcef52b6a4240430cec9c8ac91d78e1411eced56
Petr Bela <bela.petr@gmail.com>
2013-08-25 00:55:25 -0700

    docco syntax for docs

--- app/chute/api/Asset.js
+++ app/chute/api/Asset.js
@@ -1,13 +1,18 @@
 'use strict';
 
-angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUrl', 'Chute.API.Heart', function($resource, $http, apiUrl, Heart) {
+// # Chute.API.Asset
+//
+// Asset represents metadata about an image or a video.
+angular.module('chute').factory('Chute.API.Asset',
+  ['$resource', '$http', 'apiUrl', 'Chute.API.Heart', function($resource, $http, apiUrl, Heart) {
+
   var AssetResource = $resource(apiUrl + '/albums/:album/assets/:collectionRoute:id/:memberRoute', {
     album: '@album',
     id: '@id',
     collectionRoute: '@collectionRoute',
     memberRoute: '@memberRoute'
   }, {
-    // custom resource methods
+    /* custom resource methods */
 
     query: {method: 'GET', isArray: false}
   });
@@ -15,15 +20,29 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
 
   var PER_PAGE = 5;
 
+  // <a name="Assets"></a>
+  // ## Assets
+  // 
+  // Collection of assets returned by `Asset.query`.
   var Assets = {
-    /**
-    * Fetch previous page of assets and prepend them to current instance of assets.
-    * When the assets are sorted naturally (`sort` param is 'id', 'time' or none), continuous paging is used. Otherwise uses standard paging (using `page` param).
-    * @params {Function} success - will be passed these arguments:
-    *   - {Array} newly fetched assets - detached subset of the new assets (in case you need them)
-    *   - {object} response headers
-    * @params {Function} error
-    */
+    // ### Assets.prevPage
+    //
+    // Fetch previous page of assets and prepend them to current instance of assets.
+    //
+    // When the assets are sorted naturally (`sort` param is 'id', 'time' or none), continuous paging is used. Otherwise uses standard paging (using `page` param).
+    //
+    // **@params**
+    //
+    // - `success` {function} - will be passed these arguments:
+    //   - {array} detached set of newly fetched assets (in case you need them)
+    //   - {object} response headers
+    // - `error` {function}
+    //
+    // **@example**
+    // ```js
+    // var assets = Asset.query({album: 'abcqsrlx', page: 10});
+    // assets.prevPage();
+    // ```
     prevPage: function(success, error) {
       this.params.page--;
 
@@ -45,21 +64,31 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
             var asset = new AssetResource(data);
             newAssets.push(asset);
           });
-          // prepend new assets to the original array
+          /* prepend new assets to the original array */
           assets.unshift.apply(assets, newAssets);
         }
         (success||angular.noop)(newAssets, response.headers);
       }, error);
     },
 
-    /**
-    * Fetch next page of assets and append them to current instance of assets.
-    * When the assets are sorted naturally (`sort` param is 'id', 'time' or none), continuous paging is used. Otherwise uses standard paging (using `page` param).
-    * @params {Function} success - will be passed these arguments:
-    *   - {Array} newly fetched assets - detached subset of the new assets (in case you need them)
-    *   - {object} response headers
-    * @params {Function} error
-    */
+    // ### Assets.nextPage
+    //
+    // Fetch next page of assets and append them to current instance of assets.
+    //
+    // When the assets are sorted naturally (`sort` param is 'id', 'time' or none), continuous paging is used. Otherwise uses standard paging (using `page` param).
+    //
+    // **@params**
+    //
+    // - `success` {function} - will be passed these arguments:
+    //   - {array} detached set of newly fetched assets (in case you need them)
+    //   - {object} response headers
+    // - `error` {function}
+    //
+    // **@example**
+    // ```js
+    // var assets = Asset.query({album: 'abcqsrlx'});
+    // assets.nextPage();
+    // ```
     nextPage: function(success, error) {
       this.params.page++;
 
@@ -79,7 +108,7 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
             var asset = new AssetResource(data);
             newAssets.push(asset);
           });
-          // append new asssets to the original array
+          /* append new asssets to the original array */
           assets.push.apply(assets, newAssets);
         }
         if (!response || !response.data || response.data.length != (params.per_page || PER_PAGE)) {
@@ -89,33 +118,55 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
       }, error);
     },
 
-    /**
-    * Check whether there are more assets available.
-    * This method doesn't trigger any external request. The resolution is based on the result of the last query (if it returned less than requested number of items, it's a signal there are no more available).
-    * @returns {boolean}
-    *   true if there are more assets on the server;
-    *   false if we have reached the end
-    */
+    // ### Assets.hasMore
+    //
+    // Check if there are more assets available.
+    //
+    // This method doesn't trigger any external request. The resolution is based on the result of the last query (if it returned less than requested number of items, it's a signal there are no more available).
+    //
+    // **@returns** {boolean}
+    //
+    // - true if there are more assets on the server;
+    // - false if we have reached the end
+    //
+    // **@example**
+    // ```js
+    // var assets = Asset.query({album: 'abcqsrlx'});
+    // if (assets.hasMore()) {
+    //   assets.nextPage();
+    // }
+    // ```
     hasMore: function() {
       return !! this._hasMore;
     }
   };
 
+  // ## Asset.query
+  //
+  // Fetch album assets. Overwrites default `Resource.query` method, maintaining the same API.
+  // 
+  // **@params**
+  //
+  // - `params`
+  //   - `album` {string} - album shortcut like *abcqsrlx*
+  //   - `perPage` {number} - assets to return per page
+  //   - and more... see Chute API docs
+  // - `success` {function}
+  // - `error` {function}
+  //
+  // **@return** {array} [Assets](#Assets). Has additional functions (see above):
+  // 
+  // - `nextPage` - fetch next page using same params as for query
+  // - `prevPage` - fetch previous page using same params as for query
+  // - `hasMore` - whether next page of assets is available
+  // 
+  // See [AngularJS docs](http://docs.angularjs.org/api/ngResource.$resource) for more information.
+  //
+  // **@example**
+  // ```js
+  // $scope.assets = Asset.query({album: 'abcqsrlx'});
+  // ```
   AssetResource._query = AssetResource.query;
-  /**
-  * Fetch album assets. Overwrites default 'Resource.query' method, maintaining the same API.
-  *
-  * The returned array has additional functions:
-  *  - nextPage() - fetch next page using same params as for query
-  *  - prevPage() - fetch previous page using same params as for query
-  *
-  * See http://docs.angularjs.org/api/ngResource.$resource for documentation.
-  *
-  * @method query
-  * @param {object} params - query params
-  * @return Array array that will be eventually filled with assets
-  * @async
-  */
   AssetResource.query = function(params, success, error) {
     if (params.perPage) {
       params.per_page = params.perPage;
@@ -123,7 +174,7 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
     }
 
     var saveParams = angular.extend({page: 1}, params);
-    // this is a nice trick to get array with helper functions
+    /* this is a nice trick to get array with helper functions */
     var assets = angular.extend([], {params: saveParams}, Assets);
 
     AssetResource._query(params, function(response) {
@@ -144,17 +195,27 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
     return assets;
   };
 
+  // ## Asset.get
+  //
+  // Fetch asset from an album. Overwrites default `Resource.get` method, maintaining the same API.
+  // 
+  // See [AngularJS docs](http://docs.angularjs.org/api/ngResource.$resource) for more information.
+  // 
+  // **@params**
+  //
+  // - `params`
+  //   - `album` {string} - album shortcut
+  //   - `asset` {string} - asset shortcut
+  // - `success` {function}
+  // - `error` {function}
+  //
+  // **@return** {object} resource object that will be eventually filled with asset data
+  //
+  // **@example**
+  // ```js
+  // var asset = Asset.get({album: 'abcqsrlx', asset: 'vjp3miwob'});
+  // ```
   AssetResource._get = AssetResource.get;
-  /**
-  * Fetch album asset. Overwrites default 'Resource.get' method, maintaining the same API.
-  *
-  * See http://docs.angularjs.org/api/ngResource.$resource for documentation.
-  *
-  * @method get
-  * @param {object} params - query params
-  * @return Object object that will be eventually filled with asset data
-  * @async
-  */
   AssetResource.get = function(params, success, error) {
     success = (success || angular.noop);
     error   = (error   || angular.noop);
@@ -171,23 +232,28 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
     return asset;
   };  
 
-  /**
-  * Heart an asset
-  * @method heart
-  * @param {object} options - object specifying success and error callbacks
-  * @example
-      asset.heart({
-        // Success callback
-        'success': function(){
-          // asset hearted
-        },
-        // Error callback
-        'error': function(){
-          // error happened during request
-        }
-      );
-  * @async
-  */
+  // ## asset.heart
+  //
+  // Heart an asset.
+  //
+  // **@params**
+  //
+  // - `options` {object} - specify success and error callbacks
+  //
+  // **@example**
+  //
+  // ```javascript
+  // asset.heart({
+  //   // Success callback
+  //   'success': function(){
+  //     // asset hearted
+  //   },
+  //   // Error callback
+  //   'error': function(){
+  //     // error happened during request
+  //   }
+  // );
+  // ```
   AssetResource.prototype.heart = function(options) {
     options = (options || {});
       
@@ -208,23 +274,28 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
     }).error(options.error || angular.noop);
   };
 
-  /**
-  * Unheart an asset
-  * @method unheart
-  * @param {object} options - object specifying success and error callbacks
-  * @example
-      asset.unheart({
-        // Success callback
-        'success': function(){
-          // asset hearted
-        },
-        // Error callback
-        'error': function(){
-          // error happened during request
-        }
-      );
-  * @async
-  */
+  // ## Asset.unheart
+  //
+  // Unheart an asset.
+  //
+  // **@params**
+  //
+  // - `options` {object} - specify success and error callbacks
+  //
+  // **@example**
+  //
+  // ```js
+  // asset.unheart({
+  //   // Success callback
+  //   'success': function(){
+  //     // asset hearted
+  //   },
+  //   // Error callback
+  //   'error': function(){
+  //     // error happened during request
+  //   }
+  // );
+  // ```
   AssetResource.prototype.unheart = function(options) {
     options = (options || {});
 
@@ -244,34 +315,43 @@ angular.module('chute').factory('Chute.API.Asset', ['$resource', '$http', 'apiUr
     }, options.error || angular.noop);
   };
   
-  /**
-  * Check if asset was already hearted before
-  * @method hearted
-  * @return Boolean
-  * @example
-      var isHearted = asset.hearted();
-  */
+  // ## Asset.hearted
+  //
+  // Check if asset was already hearted before.
+  //
+  // **@return** {boolean}
+  //
+  // **@example**
+  //
+  // ```js
+  // var isHearted = asset.hearted();
+  // ```
   AssetResource.prototype.hearted = function() {
     return !!window.localStorage[this.album + '-' + this.shortcut + '-heart'];
   }
 
-  /**
-  * Heart if hearted, unheart if not hearted.
-  * @method toggleHeart
-  * @param {object} options - object specifying success and error callbacks
-  * @example
-      asset.toggleHeart({
-        // Success callback
-        'success': function(){
-          // asset hearted
-        },
-        // Error callback
-        'error': function(){
-          // error happened during request
-        }
-      });
-  * @async
-  */
+  // ## Asset.toggleHeart
+  //
+  // Heart if hearted, unheart if not hearted.
+  //
+  // **@params**
+  //
+  // - `options` {object} - specify success and error callbacks
+  //
+  // **@example**
+  //
+  // ```js
+  // asset.toggleHeart({
+  //   // Success callback
+  //   'success': function(){
+  //     // asset hearted
+  //   },
+  //   // Error callback
+  //   'error': function(){
+  //     // error happened during request
+  //   }
+  // });
+  // ```
   AssetResource.prototype.toggleHeart = function(options) {
     if (this.hearted()) {
       this.unheart(options);
